095e7ee4344546632d0cd4b1c2a535ea63f849e4
Artem Valkov <valkov.artyom@gmail.com>
2019-11-22 12:35:55 +0200

    simple db connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./database');
+
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +14,13 @@ app.use(bodyParser.json());
 
 app.listen(PORT, () =>  {
   console.log(`Server listening on port ${PORT}`);
+});
+
+db.query('SELECT NOW()', (err, res) => {
+  if (err.error) {
+    return console.log(err.error)
+  }
+  console.log(`PostreSQL connected ${res[0].now}.`);
 });
 
 module.exports = app;
